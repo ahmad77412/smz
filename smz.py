f0b0e6a5a95c73e3e2f2e91852a097497bd34ca5
sheraz-pirnce <109683176+sheraz-pirnce@users.noreply.github.com>
2022-07-20 22:04:38 +0500

    Update smz.py

--- smz.py
+++ smz.py
@@ -1,4 +1,17 @@
-import os
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
 os.system('git pull')
-import Smz01
-Main()
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls Enjoy\033[1;37m")
+if __name__ == "__main__":
+	try:
+		__import__("Smz01")._login()
+	except Exception as e:
+		exit(str(e))
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
