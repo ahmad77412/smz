a0dbfae0e6ade8642dcf00a328065f7e0b9e354e
sheraz-pirnce <109683176+sheraz-pirnce@users.noreply.github.com>
2022-07-21 11:58:18 +0500

    Update smz.py

--- smz.py
+++ smz.py
@@ -1,18 +1,9 @@
-import os, platform, time
-try:
-    import requests
-except:
-    os.system('pip install requests')
+import os
+import time
 os.system('git pull')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls Enjoy\033[1;37m")
-if __name__ == "__main__":
-	try:
-		__import__("dz")
-	except Exception as e:
-		exit(str(e))
-elif bit == '32bit':
-    os.system('xdg-open https://facebook.com/groups/1017905562448002/')
-    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+
+print("")
+print("\033[1;32m     Welcome To My Ok Tool");time.sleep(2.0)
+
+import Ok
+Main() 
